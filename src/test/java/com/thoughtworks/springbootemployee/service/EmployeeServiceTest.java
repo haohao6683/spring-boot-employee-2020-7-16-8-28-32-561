f1494b8453f9686e68e0cdb9ee6d1b497f735a51
haohao6683 <852721803@qq.com>
2020-07-29 15:10:40 +0800

    [Employee API][Draymond&Penny] Refactor: Refactor the should_return_employees_when_get test.

--- src/test/java/com/thoughtworks/springbootemployee/service/EmployeeServiceTest.java
+++ src/test/java/com/thoughtworks/springbootemployee/service/EmployeeServiceTest.java
@@ -3,6 +3,7 @@ package com.thoughtworks.springbootemployee.service;
 import com.thoughtworks.springbootemployee.model.Employee;
 import com.thoughtworks.springbootemployee.repository.EmployeeRepository;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -12,20 +13,27 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class EmployeeServiceTest {
-    @Test
-    void should_return_employees_when_get(){
-        //given
-        EmployeeRepository repository = mock(EmployeeRepository.class);
-        EmployeeServiceImpl employeeService = new EmployeeServiceImpl(repository);
+    private EmployeeRepository repository;
+    private EmployeeServiceImpl employeeService;
+    @BeforeEach
+    void initData(){
+        repository = mock(EmployeeRepository.class);
+        employeeService = new EmployeeServiceImpl(repository);
         List<Employee> mockedEmployees = new ArrayList<>();
         mockedEmployees.add(new Employee(1, 28, "male", "Draymond1", 1000));
         mockedEmployees.add(new Employee(2, 28, "male", "Draymond2", 100));
         mockedEmployees.add(new Employee(3, 28, "male", "Draymond3", 10));
         when(repository.getAllData()).thenReturn(mockedEmployees);
+    }
+    @Test
+    void should_return_employees_when_get(){
+        //given
+
         //when
         List<Employee> employees = employeeService.getEmployeeList();
 
         //then
         Assertions.assertEquals(repository.getAllData().size(), employees.size());
     }
+
 }
