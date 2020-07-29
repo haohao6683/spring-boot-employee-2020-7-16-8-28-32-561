1c46d75076dfdbb22e2d672ff4d32b7a72fb7f76
haohao6683 <852721803@qq.com>
2020-07-29 15:29:44 +0800

    [Employee API][Draymond&Penny] Refactor: Refactor the getAllData.

--- src/main/java/com/thoughtworks/springbootemployee/repository/EmployeeRepository.java
+++ src/main/java/com/thoughtworks/springbootemployee/repository/EmployeeRepository.java
@@ -10,9 +10,12 @@ import java.util.List;
 public class EmployeeRepository {
     public List<Employee> getAllData() {
         List<Employee> employees = new ArrayList<>();
-        employees.add(new Employee(1, 28, "male", "Draymond1", 1000));
+        employees.add(new Employee(1, 28, "female", "Draymond1", 1000));
         employees.add(new Employee(2, 28, "male", "Draymond2", 100));
-        employees.add(new Employee(3, 28, "male", "Draymond3", 10));
+        employees.add(new Employee(3, 28, "female", "Draymond3", 50));
+        employees.add(new Employee(4, 28, "male", "Draymond4", 40));
+        employees.add(new Employee(5, 28, "female", "Draymond5", 30));
+        employees.add(new Employee(6, 28, "male", "Draymond6", 20));
         return employees;
     }
 }
