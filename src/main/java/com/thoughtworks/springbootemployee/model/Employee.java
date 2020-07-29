be2dc2dbab926265c5b51e1021a0450270ba5723
haohao6683 <852721803@qq.com>
2020-07-29 17:36:29 +0800

    [Employee API][Draymond&Penny] Refactor: Configure h2 DB and JPA.

--- src/main/java/com/thoughtworks/springbootemployee/model/Employee.java
+++ src/main/java/com/thoughtworks/springbootemployee/model/Employee.java
@@ -1,16 +1,16 @@
 package com.thoughtworks.springbootemployee.model;
 
 public class Employee {
-    private int id;
-    private int age;
+    private Integer id;
+    private Integer age;
     private String gender;
     private String name;
-    private int salary;
+    private Integer salary;
 
     public Employee() {
     }
 
-    public Employee(int id, int age, String gender, String name, int salary) {
+    public Employee(Integer id, Integer age, String gender, String name, Integer salary) {
         this.id = id;
         this.age = age;
         this.gender = gender;
@@ -18,27 +18,19 @@ public class Employee {
         this.salary = salary;
     }
 
-    public int getSalary() {
-        return salary;
-    }
-
-    public void setSalary(int salary) {
-        this.salary = salary;
-    }
-
-    public int getId() {
+    public Integer getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(Integer id) {
         this.id = id;
     }
 
-    public int getAge() {
+    public Integer getAge() {
         return age;
     }
 
-    public void setAge(int age) {
+    public void setAge(Integer age) {
         this.age = age;
     }
 
@@ -56,5 +48,13 @@ public class Employee {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Integer getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Integer salary) {
+        this.salary = salary;
     }
 }
--- src/main/java/com/thoughtworks/springbootemployee/repository/EmployeeRepository.java
+++ src/main/java/com/thoughtworks/springbootemployee/repository/EmployeeRepository.java
@@ -1,21 +1,9 @@
 package com.thoughtworks.springbootemployee.repository;
 
 import com.thoughtworks.springbootemployee.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Repository
-public class EmployeeRepository {
-    public List<Employee> getAllData() {
-        List<Employee> employees = new ArrayList<>();
-        employees.add(new Employee(1, 28, "female", "Draymond1", 1000));
-        employees.add(new Employee(2, 28, "male", "Draymond2", 100));
-        employees.add(new Employee(3, 28, "female", "Draymond3", 50));
-        employees.add(new Employee(4, 28, "male", "Draymond4", 40));
-        employees.add(new Employee(5, 28, "female", "Draymond5", 30));
-        employees.add(new Employee(6, 28, "male", "Draymond6", 20));
-        return employees;
-    }
+public interface EmployeeRepository extends JpaRepository<Employee, Integer>{
 }
