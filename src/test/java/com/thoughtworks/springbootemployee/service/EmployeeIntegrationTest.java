1591267148dadaea47619b5975297f67790714d7
haohao6683 <852721803@qq.com>
2020-07-30 16:23:26 +0800

    [Draymond&York] Feature: Add should_return_employee_when_find_employees_by_gender_given_gender test case.

--- src/test/java/com/thoughtworks/springbootemployee/service/EmployeeIntegrationTest.java
+++ src/test/java/com/thoughtworks/springbootemployee/service/EmployeeIntegrationTest.java
@@ -90,5 +90,23 @@ public class EmployeeIntegrationTest {
                 .andExpect(jsonPath("$.content[0].name").value("Draymond1"))
                 .andExpect(jsonPath("$.content[0].salary").value(1000));
     }
+
+    @Test
+    void should_return_employee_when_find_employees_by_gender_given_gender() throws Exception {
+        Employee employeeA = new Employee(1, 28, "male", "Draymond1", 1000);
+        Employee employeeB = new Employee(2, 28, "female", "Draymond2", 1020);
+        List<Employee> employees = Arrays.asList(employeeA, employeeB);
+        employeeRepository.save(employeeA);
+        employeeRepository.save(employeeB);
+
+        //when then
+        mockMvc.perform(get("/employees?gender=male"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].id").isNumber())
+                .andExpect(jsonPath("$[0].age").value(28))
+                .andExpect(jsonPath("$[0].gender").value("male"))
+                .andExpect(jsonPath("$[0].name").value("Draymond1"))
+                .andExpect(jsonPath("$[0].salary").value(1000));
+    }
 }
 
