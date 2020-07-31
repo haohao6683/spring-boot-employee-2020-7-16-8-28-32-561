ebce3332f91674c7feb0cd99d29309746fe0ed0c
haohao6683 <852721803@qq.com>
2020-07-31 09:04:40 +0800

    [All][Draymond&York] Refactor: Refactor the tearDown method .

--- src/test/java/com/thoughtworks/springbootemployee/service/EmployeeIntegrationTest.java
+++ src/test/java/com/thoughtworks/springbootemployee/service/EmployeeIntegrationTest.java
@@ -3,7 +3,7 @@ package com.thoughtworks.springbootemployee.service;
 import com.thoughtworks.springbootemployee.model.Employee;
 import com.thoughtworks.springbootemployee.repository.CompanyRepository;
 import com.thoughtworks.springbootemployee.repository.EmployeeRepository;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -31,7 +31,7 @@ public class EmployeeIntegrationTest {
     @Autowired
     private EmployeeRepository employeeRepository;
 
-    @BeforeEach
+    @AfterEach
     void tearDown() {
         employeeRepository.deleteAll();
         companyRepository.deleteAll();
