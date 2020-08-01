c48b98cdfe49d59e7d599308291e54262ea3e0cd
haohao6683 <852721803@qq.com>
2020-08-02 03:49:58 +0800

    [All][Draymond] Refactor: Refactor the two integration test after code-review.

--- src/test/java/com/thoughtworks/springbootemployee/service/CompanyIntegrationTest.java
+++ src/test/java/com/thoughtworks/springbootemployee/service/CompanyIntegrationTest.java
@@ -4,6 +4,7 @@ import com.thoughtworks.springbootemployee.model.Company;
 import com.thoughtworks.springbootemployee.model.Employee;
 import com.thoughtworks.springbootemployee.repository.CompanyRepository;
 import com.thoughtworks.springbootemployee.repository.EmployeeRepository;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -29,6 +30,12 @@ public class CompanyIntegrationTest {
     @Autowired
     private EmployeeRepository employeeRepository;
 
+    @AfterEach
+    void tearDown() {
+        companyRepository.deleteAll();
+        employeeRepository.deleteAll();
+    }
+
     @Test
     void should_return_companies_when_find_all_companies_given_null() throws Exception {
         //given
@@ -48,16 +55,15 @@ public class CompanyIntegrationTest {
         mockMvc.perform(get("/companies"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$[0].id").isNumber())
-                .andExpect(jsonPath("$[0].companyName").value("OOCL"))
-                .andExpect(jsonPath("$[0].employeeNumber").value(1))
-                .andExpect(jsonPath("$[0].employees[0].name").value("OOCL1"));
+                .andExpect(jsonPath("$[0].companyName").value(company.getCompanyName()))
+                .andExpect(jsonPath("$[0].employeeNumber").value(company.getEmployeeNumber()))
+                .andExpect(jsonPath("$[0].employees[0].name").value(employees.get(0).getName()));
 
     }
 
     @Test
     void should_return_certain_company_when_find_company_by_id_given_id() throws Exception {
         //given
-        Integer id = 1;
         List<Employee> employees = Arrays.asList(
                 new Employee(1, 28, "male", "OOCL1", 1000),
                 new Employee(2, 28, "male", "OOCL2", 1000),
@@ -73,10 +79,10 @@ public class CompanyIntegrationTest {
         //when then
         mockMvc.perform(get("/companies/" + savedCompany.getId()))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.id").isNumber())
-                .andExpect(jsonPath("$.companyName").value("OOCL"))
-                .andExpect(jsonPath("$.employeeNumber").value(1))
-                .andExpect(jsonPath("$.employees[0].name").value("OOCL1"));
+                .andExpect(jsonPath("$.id").value(savedCompany.getId()))
+                .andExpect(jsonPath("$.companyName").value(savedCompany.getCompanyName()))
+                .andExpect(jsonPath("$.employeeNumber").value(savedCompany.getEmployeeNumber()))
+                .andExpect(jsonPath("$.employees[0].name").value(employees.get(0).getName()));
     }
 
     @Test
@@ -98,33 +104,31 @@ public class CompanyIntegrationTest {
         //when then
         mockMvc.perform(get("/companies/" + savedCompany.getId() + "/employees"))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$[0].name").value("OOCL1"))
-                .andExpect(jsonPath("$[1].name").value("OOCL2"))
-                .andExpect(jsonPath("$[2].name").value("OOCL3"));
+                .andExpect(jsonPath("$[0].name").value(employees.get(0).getName()))
+                .andExpect(jsonPath("$[1].name").value(employees.get(1).getName()))
+                .andExpect(jsonPath("$[2].name").value(employees.get(2).getName()));
     }
 
     @Test
     void should_return_companies_when_page_query_given_page_and_page_size() throws Exception {
         //given
-        List<Employee> employees = Arrays.asList(
-                new Employee(1, 28, "male", "OOCL1", 1000),
-                new Employee(2, 28, "male", "OOCL2", 1000),
-                new Employee(3, 28, "male", "OOCL3", 1000)
-        );
-        Company company = new Company(1,
+        Company companyA = new Company(1,
                 "OOCL",
                 1,
-                employees
+                new ArrayList<>()
         );
-        Company savedCompany = companyRepository.save(company);
+        Company savedCompanyA = companyRepository.save(companyA);
+        Company companyB = new Company(2,
+                "TW",
+                2,
+                new ArrayList<>()
+        );
+        companyRepository.save(companyB);
 
         //when then
         mockMvc.perform(get("/companies?page=0&pageSize=1"))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.content[0].id").isNumber())
-                .andExpect(jsonPath("$.content[0].companyName").value("OOCL"))
-                .andExpect(jsonPath("$.content[0].employeeNumber").value(1))
-                .andExpect(jsonPath("$.content[0].employees[0].name").value("OOCL1"));
+                .andExpect(jsonPath("$.content[0].id").value(savedCompanyA.getId()));
     }
 
     @Test
@@ -145,61 +149,59 @@ public class CompanyIntegrationTest {
                 .andExpect(jsonPath("$.id").isNumber())
                 .andExpect(jsonPath("$.companyName").value("baidu"))
                 .andExpect(jsonPath("$.employeeNumber").value(1))
-                .andExpect(jsonPath("$.employees").value(new ArrayList<>()));
+                .andExpect(jsonPath("$.employees").isEmpty());
     }
 
     @Test
     void should_return_company_when_update_company_information_given_id_and_company_information() throws Exception {
         //given
-        List<Employee> employees = Arrays.asList(
-                new Employee(1, 28, "male", "OOCL1", 1000),
-                new Employee(2, 28, "male", "OOCL2", 1000),
-                new Employee(3, 28, "male", "OOCL3", 1000)
-        );
         Company company = new Company(1,
                 "OOCL",
                 1,
-                employees
+                new ArrayList<>()
         );
         Company savedCompany = companyRepository.save(company);
 
         String updatedCompany = "{\n" +
+                "        \"id\" : " + savedCompany.getId() + ",\n" +
                 "        \"companyName\": \"baidu\",\n" +
                 "        \"employeeNumber\": 1,\n" +
                 "        \"employees\": [\n" +
-                "            \n" +
-                "        ]\n" +
+                "    {\n" +
+                "        \"age\": 29,\n" +
+                "        \"gender\": \"male\",\n" +
+                "        \"name\": \"baidu1\",\n" +
+                "        \"salary\": 100000\n" +
+                "    }\n" +
+                "]\n" +
                 "    }";
 
         //when then
         mockMvc.perform(put("/companies/" + savedCompany.getId())
                 .contentType(MediaType.APPLICATION_JSON).content(updatedCompany))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.id").isNumber())
+                .andExpect(jsonPath("$.id").value(savedCompany.getId()))
                 .andExpect(jsonPath("$.companyName").value("baidu"))
                 .andExpect(jsonPath("$.employeeNumber").value(1))
-                .andExpect(jsonPath("$.employees").value(new ArrayList<>()));
+                .andExpect(jsonPath("$.employees").isNotEmpty());
     }
 
     @Test
     void should_return_boolean_when_delete_company_by_id_given_id() throws Exception {
         //given
-        Integer id = 1;
-        List<Employee> employees = Arrays.asList(
-                new Employee(1, 28, "male", "OOCL1", 1000),
-                new Employee(2, 28, "male", "OOCL2", 1000),
-                new Employee(3, 28, "male", "OOCL3", 1000)
-        );
         Company company = new Company(1,
                 "OOCL",
                 1,
-                employees
+                new ArrayList<>()
         );
         Company savedCompany = companyRepository.save(company);
 
-        //when  then
+        //when
         mockMvc.perform(delete("/companies/" + savedCompany.getId()))
                 .andExpect(status().isOk());
-//                .andExpect(jsonPath("$").value(true));
+
+        //then
+        mockMvc.perform(get("/companies/" + savedCompany.getId()))
+                .andExpect(jsonPath("$[0].id").doesNotHaveJsonPath());
     }
 }
--- src/test/java/com/thoughtworks/springbootemployee/service/EmployeeIntegrationTest.java
+++ src/test/java/com/thoughtworks/springbootemployee/service/EmployeeIntegrationTest.java
@@ -11,9 +11,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.util.Arrays;
-import java.util.List;
-
 import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -41,17 +38,17 @@ public class EmployeeIntegrationTest {
     void should_return_employees_when_find_all_employees_given_nothing() throws Exception {
         //given
         Employee employee = new Employee(1, 28, "male", "Draymond1", 1000);
-        employeeRepository.save(employee);
+        Employee save = employeeRepository.save(employee);
 
         //when then
         mockMvc.perform(get("/employees"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$", hasSize(1)))
-                .andExpect(jsonPath("$[0].id").isNumber())
-                .andExpect(jsonPath("$[0].age").value("28"))
-                .andExpect(jsonPath("$[0].gender").value("male"))
-                .andExpect(jsonPath("$[0].name").value("Draymond1"))
-                .andExpect(jsonPath("$[0].salary").value(1000));
+                .andExpect(jsonPath("$[0].id").value(save.getId()))
+                .andExpect(jsonPath("$[0].age").value(save.getAge()))
+                .andExpect(jsonPath("$[0].gender").value(save.getGender()))
+                .andExpect(jsonPath("$[0].name").value(save.getName()))
+                .andExpect(jsonPath("$[0].salary").value(save.getSalary()));
 
     }
 
@@ -65,11 +62,11 @@ public class EmployeeIntegrationTest {
         //when then
         mockMvc.perform(get("/employees/" + savedEmployee.getId()))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.id").isNumber())
-                .andExpect(jsonPath("$.age").value(28))
-                .andExpect(jsonPath("$.gender").value("male"))
-                .andExpect(jsonPath("$.name").value("Draymond1"))
-                .andExpect(jsonPath("$.salary").value(1000));
+                .andExpect(jsonPath("$.id").value(savedEmployee.getId()))
+                .andExpect(jsonPath("$.age").value(savedEmployee.getAge()))
+                .andExpect(jsonPath("$.gender").value(savedEmployee.getGender()))
+                .andExpect(jsonPath("$.name").value(savedEmployee.getName()))
+                .andExpect(jsonPath("$.salary").value(savedEmployee.getSalary()));
 
     }
 
@@ -78,40 +75,40 @@ public class EmployeeIntegrationTest {
         //given
         Employee employeeA = new Employee(1, 28, "male", "Draymond1", 1000);
         Employee employeeB = new Employee(2, 28, "male", "Draymond2", 1020);
-        List<Employee> employees = Arrays.asList(employeeA, employeeB);
         Employee employee1 = employeeRepository.save(employeeA);
         Employee employee2 = employeeRepository.save(employeeB);
 
         //when then
-        mockMvc.perform(get("/employees?page=0&pageSize=2"))
+        mockMvc.perform(get("/employees?page=1&pageSize=1"))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.content[0].id").isNumber())
-                .andExpect(jsonPath("$.content[0].age").value(28))
-                .andExpect(jsonPath("$.content[0].gender").value("male"))
-                .andExpect(jsonPath("$.content[0].name").value("Draymond1"))
-                .andExpect(jsonPath("$.content[0].salary").value(1000));
+                .andExpect(jsonPath("$.content[0].id").value(employee2.getId()))
+                .andExpect(jsonPath("$.content[0].age").value(employee2.getAge()))
+                .andExpect(jsonPath("$.content[0].gender").value(employee2.getGender()))
+                .andExpect(jsonPath("$.content[0].name").value(employee2.getName()))
+                .andExpect(jsonPath("$.content[0].salary").value(employee2.getSalary()));
     }
 
     @Test
     void should_return_employee_when_find_employees_by_gender_given_gender() throws Exception {
+        //given
         Employee employeeA = new Employee(1, 28, "male", "Draymond1", 1000);
         Employee employeeB = new Employee(2, 28, "female", "Draymond2", 1020);
-        List<Employee> employees = Arrays.asList(employeeA, employeeB);
-        employeeRepository.save(employeeA);
+        Employee employee1 = employeeRepository.save(employeeA);
         employeeRepository.save(employeeB);
 
         //when then
-        mockMvc.perform(get("/employees?gender=male"))
+        mockMvc.perform(get("/employees?gender=" + employee1.getGender()))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$[0].id").isNumber())
-                .andExpect(jsonPath("$[0].age").value(28))
-                .andExpect(jsonPath("$[0].gender").value("male"))
-                .andExpect(jsonPath("$[0].name").value("Draymond1"))
-                .andExpect(jsonPath("$[0].salary").value(1000));
+                .andExpect(jsonPath("$[0].id").value(employee1.getId()))
+                .andExpect(jsonPath("$[0].age").value(employee1.getAge()))
+                .andExpect(jsonPath("$[0].gender").value(employee1.getGender()))
+                .andExpect(jsonPath("$[0].name").value(employee1.getName()))
+                .andExpect(jsonPath("$[0].salary").value(employee1.getSalary()));
     }
 
     @Test
-    void should_return_update_employee_when_add_employee_given_employee() throws Exception {
+    void should_return_employee_when_add_employee_given_employee() throws Exception {
+        //given
         String employeeInformation = "{\n" +
                 "    \"id\": 76,\n" +
                 "    \"age\": 28,\n" +
@@ -133,6 +130,7 @@ public class EmployeeIntegrationTest {
 
     @Test
     void should_return_updated_employee_when_update_employee_given_employee() throws Exception {
+        //given
         Employee employee = new Employee(1, 28, "male", "Draymond1", 1000);
         Employee saveEmployee = employeeRepository.save(employee);
         String employeeInformation = "{\n" +
@@ -156,12 +154,17 @@ public class EmployeeIntegrationTest {
 
     @Test
     void should_return_boolean_when_delete_employee_by_id_given_id() throws Exception {
+        //given
         Employee employee = new Employee(1, 28, "male", "Draymond1", 1000);
         Employee saveEmployee = employeeRepository.save(employee);
 
+        //when
         mockMvc.perform(delete("/employees/" + saveEmployee.getId()))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$").value(true));
+                .andExpect(status().isOk());
+
+        //then
+        mockMvc.perform(get("/employees/" + saveEmployee.getId()))
+                .andExpect(jsonPath("$[0].id").doesNotHaveJsonPath());
     }
 }
 
