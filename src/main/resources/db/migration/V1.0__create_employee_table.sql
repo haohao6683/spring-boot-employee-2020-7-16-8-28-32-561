38f7a963365ff39d7c420247d797d678e164d6da
haohao6683 <852721803@qq.com>
2020-08-02 02:15:07 +0800

    [All][Draymond] Feature: Adding the Flyway utils and some related SQL file.

---
+++ src/main/resources/db/migration/V1.0__create_employee_table.sql
@@ -0,0 +1,7 @@
+create table employee(
+    id int auto_increment primary key,
+    age int,
+    name varchar(255),
+    gender varchar(20),
+    salary int
+)
---
+++ src/main/resources/db/migration/V1.2__add_company_id_column_in_employee.sql
@@ -0,0 +1,1 @@
+alter table employee add CONSTRAINT fk_company_employee FOREIGN KEY(id) REFERENCES company (id)
