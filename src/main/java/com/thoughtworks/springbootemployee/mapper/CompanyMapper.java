2f4342f0fd503ba336b29614516fd627acf5fd64
haohao6683 <852721803@qq.com>
2020-08-02 19:18:09 +0800

    [All][Draymond] Feature: Implement test should_transform_to_companyResponse_when_given_company .

--- src/main/java/com/thoughtworks/springbootemployee/mapper/CompanyMapper.java
+++ src/main/java/com/thoughtworks/springbootemployee/mapper/CompanyMapper.java
@@ -1,6 +1,7 @@
 package com.thoughtworks.springbootemployee.mapper;
 
 import com.thoughtworks.springbootemployee.dto.CompanyRequest;
+import com.thoughtworks.springbootemployee.dto.CompanyResponse;
 import com.thoughtworks.springbootemployee.model.Company;
 import org.springframework.beans.BeanUtils;
 
@@ -12,5 +13,11 @@ public class CompanyMapper {
         Company company = new Company();
         BeanUtils.copyProperties(companyRequest, company);
         return company;
+    }
+
+    public CompanyResponse toCompanyResponse(Company company) {
+        CompanyResponse companyResponse = new CompanyResponse();
+        BeanUtils.copyProperties(company, companyResponse);
+        return companyResponse;
     }
 }
