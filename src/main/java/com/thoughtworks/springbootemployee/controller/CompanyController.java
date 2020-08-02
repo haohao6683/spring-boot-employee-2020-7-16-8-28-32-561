b750039cf283883ff661e7215d86823e4d70cccc
haohao6683 <852721803@qq.com>
2020-08-02 18:10:11 +0800

    [All][Draymond] Refactor: Refactor the CompanyController when added the GlobalExceptionHandler.

--- src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
@@ -1,5 +1,7 @@
 package com.thoughtworks.springbootemployee.controller;
 
+import com.thoughtworks.springbootemployee.Exception.IllegalOperationException;
+import com.thoughtworks.springbootemployee.Exception.NoSuchDataException;
 import com.thoughtworks.springbootemployee.model.Company;
 import com.thoughtworks.springbootemployee.model.Employee;
 import com.thoughtworks.springbootemployee.service.CompanyServiceImpl;
@@ -7,8 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
@@ -17,91 +17,18 @@ public class CompanyController {
     @Autowired
     private CompanyServiceImpl companyService;
 
-    private List<Company> initCompanies() {
-        List<Company> companies = new ArrayList<>();
-        companies.add(
-                new Company(1,
-                        "OOCL",
-                        1,
-                        Arrays.asList(
-                                new Employee(1, 28, "male", "OOCL1", 1000),
-                                new Employee(2, 28, "male", "OOCL2", 1000),
-                                new Employee(3, 28, "male", "OOCL3", 1000)
-                        )
-                )
-        );
-        companies.add(
-                new Company(2,
-                        "CargoSmart",
-                        1,
-                        Arrays.asList(
-                                new Employee(1, 28, "male", "CargoSmart1", 1000),
-                                new Employee(2, 28, "male", "CargoSmart2", 1000),
-                                new Employee(3, 28, "male", "CargoSmart3", 1000)
-                        )
-                )
-        );
-        companies.add(
-                new Company(3,
-                        "KFC",
-                        1,
-                        Arrays.asList(
-                                new Employee(1, 28, "male", "KFC1", 1000),
-                                new Employee(2, 28, "male", "KFC2", 1000),
-                                new Employee(3, 28, "male", "KFC3", 1000)
-                        )
-                )
-        );
-        companies.add(
-                new Company(4,
-                        "MC",
-                        1,
-                        Arrays.asList(
-                                new Employee(1, 28, "male", "MC1", 1000),
-                                new Employee(2, 28, "male", "MC2", 1000),
-                                new Employee(3, 28, "male", "MC3", 1000)
-                        )
-                )
-        );
-        companies.add(
-                new Company(5,
-                        "alibaba",
-                        1,
-                        Arrays.asList(
-                                new Employee(1, 28, "male", "alibaba1", 1000),
-                                new Employee(2, 28, "male", "alibaba2", 1000),
-                                new Employee(3, 28, "male", "alibaba3", 1000)
-                        )
-                )
-        );
-        companies.add(
-                new Company(6,
-                        "Tencent",
-                        1,
-                        Arrays.asList(
-                                new Employee(1, 28, "male", "Tencent1", 1000),
-                                new Employee(2, 28, "male", "Tencent2", 1000),
-                                new Employee(3, 28, "male", "Tencent3", 1000)
-                        )
-                )
-        );
-
-
-        return companies;
-    }
-
     @GetMapping
     public List<Company> getAllCompanies() {
         return companyService.getCompanyList();
     }
 
     @GetMapping("/{id}")
-    public Company getCompaniesById(@PathVariable int id) {
+    public Company getCompaniesById(@PathVariable int id) throws NoSuchDataException {
         return companyService.findById(id);
     }
 
     @GetMapping("/{id}/employees")
-    public List<Employee> getEmployeesByCompanyId(@PathVariable int id) {
+    public List<Employee> getEmployeesByCompanyId(@PathVariable int id) throws NoSuchDataException {
         return companyService.findEmployeesByCompanyId(id);
     }
 
@@ -111,17 +38,17 @@ public class CompanyController {
     }
 
     @PostMapping
-    public Company addCompany(@RequestBody Company newCompany) {
+    public Company addCompany(@RequestBody Company newCompany) throws IllegalOperationException {
         return companyService.addCompany(newCompany);
     }
 
     @PutMapping("/{id}")
-    public Company updateCompanyByID(@PathVariable int id, @RequestBody Company newCompany) {
-        return companyService.updateCompanyByID(id,newCompany);
+    public Company updateCompanyByID(@PathVariable int id, @RequestBody Company newCompany) throws IllegalOperationException {
+        return companyService.updateCompanyByID(id, newCompany);
     }
 
     @DeleteMapping("/{id}")
-    public void deleteAllEmployeesOfTheCompanyByID(@PathVariable int id) {
+    public void deleteAllEmployeesOfTheCompanyByID(@PathVariable int id) throws IllegalOperationException {
         companyService.deleteCompanyByID(id);
     }
 }
