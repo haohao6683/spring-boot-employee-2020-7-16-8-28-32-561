237508da90e3f031dbce4d23adc342bb81649bce
haohao6683 <852721803@qq.com>
2020-08-02 19:04:27 +0800

    [All][Draymond] Feature: Implement test should_transform_to_employeeResponse_when_given_employee .

---
+++ src/main/java/com/thoughtworks/springbootemployee/dto/EmployeeResponse.java
@@ -0,0 +1,61 @@
+package com.thoughtworks.springbootemployee.dto;
+
+public class EmployeeResponse {
+    private Integer id;
+    private Integer age;
+    private String gender;
+    private String name;
+    private Integer salary;
+
+    public EmployeeResponse(Integer id, Integer age, String gender, String name, Integer salary) {
+        this.id = id;
+        this.age = age;
+        this.gender = gender;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public EmployeeResponse() {
+
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Integer salary) {
+        this.salary = salary;
+    }
+}
