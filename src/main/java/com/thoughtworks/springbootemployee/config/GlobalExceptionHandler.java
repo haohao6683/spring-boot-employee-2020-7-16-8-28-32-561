077b9a65f31c6dffa8e770ef650110d7c9b26a8b
haohao6683 <852721803@qq.com>
2020-08-02 18:05:39 +0800

    [All][Draymond] Feature: Add GlobalExceptionHandler.

---
+++ src/main/java/com/thoughtworks/springbootemployee/config/GlobalExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.thoughtworks.springbootemployee.config;
+
+import com.thoughtworks.springbootemployee.Exception.IllegalOperationException;
+import com.thoughtworks.springbootemployee.Exception.NoSuchDataException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    public static final String NO_SUCH_DATA = "No such data!";
+    public static final String ILLEGAL_OPERATION = "Illegal operation!";
+
+    @ResponseBody
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NoSuchDataException.class)
+    String handleNoSuchDataException() {
+        return NO_SUCH_DATA;
+    }
+
+    @ResponseBody
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(IllegalOperationException.class)
+    String handleIllegalOperationException() {
+        return ILLEGAL_OPERATION;
+    }
+}
